419b1d41552d5e6b699f4ac6cce38b6fa1241a50
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-31 00:00:05 +0600

    `6.11` added

--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { filterActions } from "../../features/filter/filterSlice";
+
 export default function Search() {
+    const search = useSelector((state) => state.search);
+    const dispatch = useDispatch();
+
+    const [input, setInput] = useState(search);
+
+    const onSearch = (e) => {
+        e.preventDefault();
+        dispatch(filterActions.searched(input));
+    };
+
     return (
-        <form>
+        <form onSubmit={onSearch}>
             <input
                 className="outline-none border-none mr-2"
                 type="search"
                 name="search"
                 placeholder="Search"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
             />
         </form>
     );
